b6016db2637246c0b38079f079a5389abdd20742
simon.webok@gmail.com <simon.webok@gmail.com>
2018-05-30 21:54:53 +0800

    增加博客目录插件 - tocify

--- footer.php
+++ footer.php
@@ -6,18 +6,21 @@
 
 
   <script src="<?php $this->options->themeUrl('js/jquery.min.js'); ?>"></script>
+  <?php if ($this->is('post')) : ?>
+  <script src="<?php $this->options->themeUrl('js/jquery-ui-1.9.1.custom.min.js'); ?>"></script>
+  <?php endif; ?>
   <script src="<?php $this->options->themeUrl('js/bootstrap.js'); ?>"></script>
   <script src="<?php $this->options->themeUrl('js/app.js'); ?>"></script>
   <script src="<?php $this->options->themeUrl('js/jquery.slimscroll.min.js'); ?>"></script>
   <script src="<?php $this->options->themeUrl('js/app.plugin.js'); ?>"></script>
   <script>
-  var _hmt = _hmt || [];
-  (function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?2931b314f9d3d661167ff86b21ad9005";
-    var s = document.getElementsByTagName("script")[0]; 
-    s.parentNode.insertBefore(hm, s);
-  })();
+    var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?2931b314f9d3d661167ff86b21ad9005";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();
   </script>
   <script>
     $('.dropdown-stop').click(function(e) {
@@ -35,6 +38,23 @@
       });
   });
   </script>
+
+<?php if ($this->is('post')) : ?>
+    <script src="<?php $this->options->themeUrl('js/jquery.tocify.min.js'); ?>"></script>
+    <script>
+        $(function() {
+            var toc = $("#toc").tocify({
+              selectors: "h3,h4"
+            }).data("toc-tocify");
+            
+
+        });
+    </script> 
+<?php endif; ?>
+
+
+  
+
   <?php $this->footer(); ?>
  </body>
 </html>
--- sidebarRight.php
+++ sidebarRight.php
@@ -4,9 +4,7 @@
 <h5 class="font-bold"><?php _e('热门文章'); ?></h5> 
 
 <div class="blog-ad-box m-b-lg"> 
-
 <?php TePostViews_Plugin::outputHotPosts($this) ?>
-
 </div>
 
 <h5 class="font-bold"><?php _e('便捷分类'); ?></h5> 
@@ -15,13 +13,18 @@
                    ->parse('<li class="list-group-item"> <a href="{permalink}"> <span class="badge pull-right">{count}</span> {name} </a> </li>'); ?>
 </ul> 
 
+<?php if ($this->is('post')) : ?>
+    <h5 class="font-bold"><?php _e('文章目录'); ?></h5> 
+    <div id="toc"></div>
+<?php endif; ?>
+
 <h5 class="font-bold"><?php _e('热门标签'); ?></h5> 
 <div class="tags m-b-lg l-h-2x"> 
 <?php $this->widget('Widget_Metas_Tag_Cloud', 'ignoreZeroCount=1&limit=28')->to($tags); ?>
 <?php while($tags->next()): ?>
 <a href="<?php $tags->permalink(); ?>" class="label bg-primary"><?php $tags->name(); ?></a>
 <?php endwhile; ?>
-
 </div> 
+
 </div> 
 <!-- / SidebarRight -->
