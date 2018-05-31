1ac4a6bfbe51657e448e82c6dcecfa796ea6a6de
simon.webok <simon.webok@gmail.com>
2018-05-31 20:07:09 +0800

    tocify right css

--- footer.php
+++ footer.php
@@ -4,29 +4,18 @@
    </section> 
   </section> <!-- / .vbox -->
 
-
   <script src="<?php $this->options->themeUrl('js/jquery.min.js'); ?>"></script>
-  <?php if ($this->is('post')) : ?>
-  <script src="<?php $this->options->themeUrl('js/jquery-ui-1.9.1.custom.min.js'); ?>"></script>
-  <?php endif; ?>
   <script src="<?php $this->options->themeUrl('js/bootstrap.js'); ?>"></script>
   <script src="<?php $this->options->themeUrl('js/app.js'); ?>"></script>
   <script src="<?php $this->options->themeUrl('js/jquery.slimscroll.min.js'); ?>"></script>
   <script src="<?php $this->options->themeUrl('js/app.plugin.js'); ?>"></script>
-  <script>
-    var _hmt = _hmt || [];
-    (function() {
-      var hm = document.createElement("script");
-      hm.src = "https://hm.baidu.com/hm.js?2931b314f9d3d661167ff86b21ad9005";
-      var s = document.getElementsByTagName("script")[0]; 
-      s.parentNode.insertBefore(hm, s);
-    })();
-  </script>
+
   <script>
     $('.dropdown-stop').click(function(e) {
         e.stopPropagation();
     });
   </script>
+
   <script>
     $(document).ready(function(){
       var comment_html = document.getElementById("comment");
@@ -39,23 +28,27 @@
   });
   </script>
 
-<?php if ($this->is('post')) : ?>
-    <script src="<?php $this->options->themeUrl('js/jquery.tocify.min.js'); ?>"></script>
-    <script>
-        $(function() {
-            var toc = $("#toc").tocify({
-              selectors: "h3,h4"
-            }).data("toc-tocify");
-            
-            prettyPrint();
-            $(".optionName").popover({ trigger: "hover" });
+  <?php if ($this->is('post')) : ?>
+      <script src="<?php $this->options->themeUrl('js/jquery.tocify.min.js'); ?>"></script>
+      <script>
+          // 滚动对象应该是窗口才可以，去掉html上的app即可
+          $(function() {
+              var toc = $("#toc").tocify({
+                selectors: "h2,h3,h4,h5"
+              }).data("toc-tocify");
+          });
+      </script> 
+  <?php endif; ?>
 
-        });
-    </script> 
-<?php endif; ?>
-
-
-  
+  <script>
+    var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?2931b314f9d3d661167ff86b21ad9005";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();
+  </script>
 
   <?php $this->footer(); ?>
  </body>
--- sidebarRight.php
+++ sidebarRight.php
@@ -1,30 +1,32 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <!-- SidebarRight -->
-<div class="col-sm-3"> 
-<h5 class="font-bold"><?php _e('热门文章'); ?></h5> 
+    <div class="col-sm-3"> 
+    <h5 class="font-bold"><?php _e('热门文章'); ?></h5> 
 
-<div class="blog-ad-box m-b-lg"> 
-<?php TePostViews_Plugin::outputHotPosts($this) ?>
-</div>
+    <div class="blog-ad-box m-b-lg"> 
+    <?php TePostViews_Plugin::outputHotPosts($this) ?>
+    </div>
 
-<h5 class="font-bold"><?php _e('便捷分类'); ?></h5> 
-<ul class="list-group blog-category-box m-b-lg"> 
-   <?php $this->widget('Widget_Metas_Category_List')
-                   ->parse('<li class="list-group-item"> <a href="{permalink}"> <span class="badge pull-right">{count}</span> {name} </a> </li>'); ?>
-</ul> 
+    <h5 class="font-bold"><?php _e('便捷分类'); ?></h5> 
+    <ul class="list-group blog-category-box m-b-lg"> 
+    <?php $this->widget('Widget_Metas_Category_List')
+                    ->parse('<li class="list-group-item"> <a href="{permalink}"> <span class="badge pull-right">{count}</span> {name} </a> </li>'); ?>
+    </ul> 
 
-<?php if ($this->is('post')) : ?>
-    <h5 class="font-bold"><?php _e('文章目录'); ?></h5> 
-    <div id="toc"></div>
-<?php endif; ?>
+    <?php if ($this->is('post')) : ?>
+        <div class="blog-tocify-right m-b-lg">
+            <h5 class="font-bold"><?php _e('文章目录'); ?></h5> 
+            <div id="toc"></div>
+        </div>
+    <?php endif; ?>
 
-<h5 class="font-bold"><?php _e('热门标签'); ?></h5> 
-<div class="tags m-b-lg l-h-2x"> 
-<?php $this->widget('Widget_Metas_Tag_Cloud', 'ignoreZeroCount=1&limit=28')->to($tags); ?>
-<?php while($tags->next()): ?>
-<a href="<?php $tags->permalink(); ?>" class="label bg-primary"><?php $tags->name(); ?></a>
-<?php endwhile; ?>
-</div> 
+    <h5 class="font-bold"><?php _e('热门标签'); ?></h5> 
+    <div class="tags m-b-lg l-h-2x"> 
+    <?php $this->widget('Widget_Metas_Tag_Cloud', 'ignoreZeroCount=1&limit=28')->to($tags); ?>
+    <?php while($tags->next()): ?>
+    <a href="<?php $tags->permalink(); ?>" class="label bg-primary"><?php $tags->name(); ?></a>
+    <?php endwhile; ?>
+    </div> 
 
 </div> 
 <!-- / SidebarRight -->
