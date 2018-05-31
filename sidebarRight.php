a74a3278574a49ee91b71b06b1109b7b0982abc8
simon.webok@gmail.com <simon.webok@gmail.com>
2018-05-31 22:42:54 +0800

    tocify css,html

--- sidebarRight.php
+++ sidebarRight.php
@@ -13,13 +13,6 @@
                     ->parse('<li class="list-group-item"> <a href="{permalink}"> <span class="badge pull-right">{count}</span> {name} </a> </li>'); ?>
     </ul> 
 
-    <?php if ($this->is('post')) : ?>
-        <div class="blog-tocify-right m-b-lg">
-            <h5 class="font-bold"><?php _e('文章目录'); ?></h5> 
-            <div id="toc"></div>
-        </div>
-    <?php endif; ?>
-
     <h5 class="font-bold"><?php _e('热门标签'); ?></h5> 
     <div class="tags m-b-lg l-h-2x"> 
     <?php $this->widget('Widget_Metas_Tag_Cloud', 'ignoreZeroCount=1&limit=28')->to($tags); ?>
@@ -28,5 +21,12 @@
     <?php endwhile; ?>
     </div> 
 
+    <?php if ($this->is('post')) : ?>
+        <div class="blog-tocify-right m-b-lg">
+            <h5 class="font-bold"><?php _e('文章目录'); ?></h5> 
+            <div id="toc"></div>
+        </div>
+    <?php endif; ?>
+    
 </div> 
 <!-- / SidebarRight -->
